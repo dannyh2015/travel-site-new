919b1c5f989b30d4cb4af22605d0745f1b12e96c
Danny <danhar2005@googlemail.com>
2020-05-21 15:29:41 +0100

    Completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,42 @@
-import '../styles/styles.css'
+import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
-let stickyHeader = new StickyHeader();
+ new StickyHeader();
+let modal;
+
+const newLocal = "The page was opened";
+console.log(newLocal);
+
+newFunction();
 
 if (module.hot) {
     module.hot.accept()
 }
 
-
+function newFunction() {
+    document.querySelectorAll(".open-modal").forEach(el => {
+        console.log("something was found");
+        el.addEventListener("click", e => {
+            console.log("something was clicked");
+            e.preventDefault();
+            if (typeof modal == "undefined") {
+                console.log("modal not defined");
+                import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                    modal = new x.default();
+                    setTimeout(() => modal.openTheModal(), 20);
+                }).catch(() => console.log("There was a problem"));
+            }
+            else {
+                modal.openTheModal();
+            }
+        });
+    });
+}
 
