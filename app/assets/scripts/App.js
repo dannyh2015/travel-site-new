1bf305dacd5898087ffd69baa613f12e9735a8c2
Danny <danhar2005@googlemail.com>
2020-05-22 11:26:01 +0100

    Completed lazyloading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
@@ -8,35 +9,33 @@ new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
- new StickyHeader();
+new StickyHeader();
 let modal;
 
 const newLocal = "The page was opened";
 console.log(newLocal);
 
-newFunction();
+
 
 if (module.hot) {
     module.hot.accept()
 }
 
-function newFunction() {
-    document.querySelectorAll(".open-modal").forEach(el => {
-        console.log("something was found");
-        el.addEventListener("click", e => {
-            console.log("something was clicked");
-            e.preventDefault();
-            if (typeof modal == "undefined") {
-                console.log("modal not defined");
-                import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
-                    modal = new x.default();
-                    setTimeout(() => modal.openTheModal(), 20);
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    console.log("something was found");
+    el.addEventListener("click", e => {
+        console.log("something was clicked");
+        e.preventDefault();
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
                 }).catch(() => console.log("There was a problem"));
-            }
-            else {
-                modal.openTheModal();
-            }
-        });
+        }else {
+            modal.openTheModal();
+        }
     });
-}
+});
+
 
